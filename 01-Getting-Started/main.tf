580999e9ad5a1db5ed6735e53c9ea5ee0d8e533c
Ubuntu <ubuntu@ip-172-31-0-91.ap-south-1.compute.internal>
2026-08-02 08:03:58 +0000

    Completed Terraform Phase 1 and Phase 2

---
+++ 01-Getting-Started/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.15.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-demo-instance-v2"
+  }
+}
